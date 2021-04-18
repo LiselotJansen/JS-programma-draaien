a52e19f2dc0388bc6cb9bddf9290b5e410fae51e
LiselotJansen <80678991+LiselotJansen@users.noreply.github.com>
2021-04-18 22:25:03 +0200

    Add files via upload

---
+++ Opdracht2_JS programma draaien/script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Winc Academy studenten")
+// Dit is een grote som. Dit zie ik niet in node omdat het een comment is. 
+/* 
+let addition = 1230941 * 1823794;
+console.log(addition);
+let division = 637263 / 54;
+console.log(division);
+*/
+
+
